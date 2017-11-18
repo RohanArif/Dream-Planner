65a238f9bd4f11ab6196b22da4e1e2a41dcebf98
Carrin <chinacarrin112@outlook.com>
2017-11-18 20:36:05 +0100

    Add new project[practice]

---
+++ Dream Planner/Dream Planner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dream_Planner.Startup))]
+namespace Dream_Planner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
